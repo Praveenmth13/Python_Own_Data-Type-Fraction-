c1d51b4266be078e54cf4f38abe3b81d82f8600a
Praveen Kumar <72121316+Praveenmth13@users.noreply.github.com>
2022-06-22 00:58:53 +0530

    Add files via upload

---
+++ Fraction Libraries.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from Fraction import Fraction
+x = Fraction(3,4)
+y = Fraction(5,4)
+print(x)
+print(y)
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+
